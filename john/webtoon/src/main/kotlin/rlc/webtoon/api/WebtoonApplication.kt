86f4e90295f36dd3c7e9583ff6502b60949a7d35
crinkj <jcrinkey89120@gmail.com>
2024-02-15 11:59:55 +0900

    init: john 프로젝트 기본세팅

---
+++ john/webtoon/src/main/kotlin/rlc/webtoon/api/WebtoonApplication.kt
@@ -0,0 +1,11 @@
+package rlc.webtoon.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebtoonApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebtoonApplication>(*args)
+}
