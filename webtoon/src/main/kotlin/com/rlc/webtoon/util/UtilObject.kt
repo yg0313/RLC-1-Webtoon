04a9b27182df3f83c80602f8da67e989ff101908
Kim yonggwan <sssw0101@naver.com>
2024-02-21 00:57:22 +0900

    Revert "feat: API 응답 포맷 및 jsonMapper 추가"
    This reverts commit fd500a504939018c58ab3235926120e9e3a06a41.

--- webtoon/src/main/kotlin/com/rlc/webtoon/util/UtilObject.kt
+++
@@ -1,6 +0,0 @@
-package com.rlc.webtoon.util
-
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-
-val jsonMapper = ObjectMapper().registerKotlinModule() //jacksonMapper
